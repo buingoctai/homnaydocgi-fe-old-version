8fd540513318ac4c9bf4ca781359c4f322180086
buingoctai <buingoctai994@gmail.com>
2020-04-19 15:32:27 +0700

    update build

---
+++ build/precache-manifest.ef85e4100df06c4baeef25c73733d915.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c987d9eea9cfdfa0cce1879e7d9d2bbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f923ec1769c111f4a936",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7bdd5b7db1ed9b297855",
+    "url": "/static/js/2.68f97944.chunk.js"
+  },
+  {
+    "revision": "f60437bb896b654c0fdf82e335f1d9c8",
+    "url": "/static/js/2.68f97944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f923ec1769c111f4a936",
+    "url": "/static/js/main.a5bb53cc.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  },
+  {
+    "revision": "c743f7e892887c7cfa5748d7ecae38a6",
+    "url": "/static/media/avatar.c743f7e8.jpg"
+  }
+]);
