1c396ff3229763d0405ccf880d51430970dfda8b
Quang Vinh <zeimier@gmail.com>
2020-07-31 00:03:36 +0700

    fix register

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -38,22 +38,23 @@ export function register(config) {
       const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
       console.log("swUrl=", swUrl);
 
-      if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still exists or not.
-        checkValidServiceWorker(swUrl, config);
+      // if (isLocalhost) {
+      //   // This is running on localhost. Let's check if a service worker still exists or not.
+      //   checkValidServiceWorker(swUrl, config);
 
-        // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
-        navigator.serviceWorker.ready.then(() => {
-          console.log(
-            "This web app is being served cache-first by a service " +
-              "worker. To learn more, visit https://bit.ly/CRA-PWA"
-          );
-        });
-      } else {
-        // Is not localhost. Just register service worker
-        registerValidSW(swUrl, config);
-      }
+      //   // Add some additional logging to localhost, pointing developers to the
+      //   // service worker/PWA documentation.
+      //   navigator.serviceWorker.ready.then(() => {
+      //     console.log(
+      //       "This web app is being served cache-first by a service " +
+      //         "worker. To learn more, visit https://bit.ly/CRA-PWA"
+      //     );
+      //   });
+      // } else {
+      //   // Is not localhost. Just register service worker
+      //   registerValidSW(swUrl, config);
+      // }
+      registerValidSW(swUrl, config);
     });
   }
 }
